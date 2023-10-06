8510531cfc0c460e0b9df76f8701def67d9280e8
FreyaRhianna <freyarsh@gmail.com>
2023-10-06 09:44:55 +0100

    Fixed using global environment variables

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,13 @@ module DungeonsAndDevelopment
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
 
+    config.before_configuration do
+      env_file = File.join(Rails.root, 'config', 'local_env.yml')
+      YAML.load(File.open(env_file)).each do |key, value|
+        ENV[key.to_s] = value
+      end if File.exist?(env_file)
+    end
+
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
