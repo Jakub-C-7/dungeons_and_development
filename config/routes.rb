b1ebe2c8e1666702978182e14d9cc1b0cc4f0a4d
Jakub-C-7 <jakubchamera7@gmail.com>
2023-10-08 18:01:04 +0100

    added new about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'main#home'
+  get '/about', to: 'main#about'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
