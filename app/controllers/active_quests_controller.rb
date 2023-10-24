dbf12f274ac3346c4fa56dac6baa52c7feaba6ec
Jakub-C-7 <jakubchamera7@gmail.com>
2023-10-24 15:27:17 +0100

    active quest controller added and basic info retrieved for the front-end

--- app/controllers/active_quests_controller.rb
+++ app/controllers/active_quests_controller.rb
@@ -1,5 +1,13 @@
 class ActiveQuestsController < ApplicationController
   def home
+    @currently_active_pathways = current_user.pathways;
+    # @current_pathways = current_user.user_pathways.where(progress: "started");
+    # @current_pathways = current_user.user_pathways.where(progress: "started").joins(:pathway).order(:id);
+    @current_pathways = current_user.user_pathways.joins(:pathway).where(progress: "started").select('pathways.*,user_pathways.*');
+
+    @currently_active_sections = current_user.sections.pluck(:id, :name, :description);
+    @currently_active_tasks = current_user.tasks.pluck(:id, :name, :description);
+
   end
 
   def show
