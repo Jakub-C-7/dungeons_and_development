dd307bfb2798963d1aee32e35018fa73ea9773b9
FreyaRhianna <freyarsh@gmail.com>
2023-10-28 14:57:36 +0100

    Added seed data for equipment and renamed type to category as it was reserved

--- app/models/equipment.rb
+++ app/models/equipment.rb
@@ -2,4 +2,12 @@ class Equipment < ApplicationRecord
   belongs_to :pathway
   has_many :user_equipments
   has_many :users, through: :user_equipments 
+
+  enum category: {
+    helmet: 0,
+    weapon: 1,
+    chest: 2,
+    bottoms: 3,
+    decor: 4 
+  }
 end
