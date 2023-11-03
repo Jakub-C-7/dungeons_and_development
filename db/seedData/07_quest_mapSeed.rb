34e69ba9c90127b86bdd4309def306722fb982ac
Jakub-C-7 <jakubchamera7@gmail.com>
2023-11-03 21:06:33 +0000

    adjusted map icon placement

--- db/seedData/07_quest_mapSeed.rb
+++ db/seedData/07_quest_mapSeed.rb
@@ -2,7 +2,7 @@ QuestMap.destroy_all
 
 
 QuestMap.create(image: "Size3Map.svg", size: 3, coordinates_left: [29, 56 ,78], coordinates_bot: [47, 63, 32])
-QuestMap.create(image: "Size5Map.svg", size: 5, coordinates_left: [16, 35 ,45, 54, 15], coordinates_bot: [15, 15, 35, 28, 73])
+QuestMap.create(image: "Size5Map.svg", size: 5, coordinates_left: [18, 38 ,48, 56, 76], coordinates_bot: [33, 33, 52, 46, 33])
 
 
 p "Finished quest_map seed"
