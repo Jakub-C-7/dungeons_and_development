2c8a4193dc8fc843590f0211d58da7ef67fe684e
Jakub-C-7 <jakubchamera7@gmail.com>
2023-11-05 13:48:37 +0000

    adjusted size 3 map icon location

--- db/seedData/07_quest_mapSeed.rb
+++ db/seedData/07_quest_mapSeed.rb
@@ -1,7 +1,7 @@
 QuestMap.destroy_all
 
 
-QuestMap.create(image: "Size3Map.svg", size: 3, coordinates_left: [29, 56 ,78], coordinates_bot: [47, 63, 32])
+QuestMap.create(image: "Size3Map.svg", size: 3, coordinates_left: [34, 63 ,77], coordinates_bot: [57, 65, 44])
 QuestMap.create(image: "Size5Map.svg", size: 5, coordinates_left: [18, 38 ,48, 56, 76], coordinates_bot: [33, 33, 52, 46, 33])
 
 
